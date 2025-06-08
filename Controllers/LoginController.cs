152e3844632b8747d92641a9b08abb6cc76ce058
louisssx <joshualouis.s.bernales@gmail.com>
2025-06-08 12:51:22 +0800

    Add project files.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using APPDEV_WebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APPDEV_WebApplication.Models
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(LoginModel model)
+        {
+            // Dummy hardcoded login
+            if (model.Username == "admin" && model.Password == "password")
+            {
+                ViewBag.Message = "Login successful!";
+                return View("Success");
+            }
+
+            ViewBag.Message = "Invalid username or password.";
+            return View();
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace APPDEV_WebApplication.Models
+{
+    public class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
